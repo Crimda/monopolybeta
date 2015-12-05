c575cbd4be98c77927ea9c633de6275daaba4992
Crimda <theblues92@gmail.com>
2015-12-05 18:46:52 -0500

    added file

---
+++ monopoly/src/Board.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Brad
+ */
+public class Board 
+{//I have no idea what I'm doing to create a rectangluar board 
+    int presentTurn = 0;
+    int playerCount = 0;
+    Player[] players; 
+    Space[] spaces = new Space[40];
+    
+    
+}
