65f68a535311a6f2a05d67e294c2f4970baf307d
Crimda <theblues92@gmail.com>
2015-12-06 15:16:16 -0500

    added go tile

---
+++ monopoly/src/Go.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brad
+ */
+public class Go extends Space 
+{
+    public Go(String name) 
+    {
+	super(name);
+    }
+	
+    @Override
+    public void action(Player player, Board board) 
+    {
+	System.out.println(player.getName() + " is at Go, get's 200. ");
+        player.getMoney().addMoney(200); //add 200 to player's total 
+    }
+    
+    //TODO: How to give for passing go and not landing? 
+}
+
