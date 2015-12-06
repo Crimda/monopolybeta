f120ad07bb846ab5221048b8128178f0d9c3665c
Crimda <theblues92@gmail.com>
2015-12-05 21:40:08 -0500

    added partial functions

--- monopoly/src/Board.java
+++ monopoly/src/Board.java
@@ -3,11 +3,81 @@
  * @author Brad
  */
 public class Board 
-{//I have no idea what I'm doing to create a rectangluar board 
+{
     int presentTurn = 0;
     int playerCount = 0;
     Player[] players; 
     Space[] spaces = new Space[40];
+    //TODO: need a way to name/mark the spaces 
     
+    /**
+    public Board()
+    {
+    }
+    TODO: call functions for special blocks like jail, and rent, and everything  else kill me now
+    */
+    
+    public Space movePos(Player player, int roll)
+    {
+        if(player.isBankrupt())
+        {
+            return spaces[player.getPos()];
+            //TODO: Handle bankruptcy and eject player? 
+        }
+        
+        int newPos = (player.getPos() + roll);
+        player.setPos(newPos);
+        System.out.println("Player moves to " + spaces[player.getPos()].getName()); //TODO: get a helper function to identify player name and print it
+        return spaces[newPos];
+    }
+    
+    public boolean hasWinner() 
+    {//see if there's only 1 player left
+	int stillAlive = 0;
+	for(Player player:players)
+        {
+            if(!player.isBankrupt())
+            {
+                stillAlive++;
+            }
+	}
+            return stillAlive <= 1;
+    }
+	
+    public Player getWinner() 
+    {//get the remaining player 
+	if(!hasWinner())
+        { 
+            return null; 
+        }
+	for(Player player:players)
+        {
+            if(!player.isBankrupt())
+            { 
+                return player; 
+            }
+	}
+		return null;
+    }
+    
+    public Player getCurrentPlayer() 
+    {
+        return players[presentTurn];
+    }
+	
+    public Player[] getPlayers() 
+    {
+        return players;
+    }
+	
+    public Player getPlayer(int id) 
+    {
+	return players[id];
+    }
+	
+    public int getTotalSpaces() 
+    {
+	return spaces.length;
+    }
     
 }
