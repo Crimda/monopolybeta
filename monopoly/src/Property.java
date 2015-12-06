01b2153fdbe23b36183c4afdbad4e639fe0780cf
Crimda <theblues92@gmail.com>
2015-12-06 17:15:05 -0500

    added incomplete property class
    the action function needs to check if the property is owned, if not, ask
    if player wants to buy. If it is owned, it should charge the landing
    player rent

---
+++ monopoly/src/Property.java
@@ -0,0 +1,38 @@
+
+/**
+ *
+ * @author Brad
+ */
+public class Property extends Space
+{
+    int price; 
+    int master = -1;
+    
+    public Property(String name, int price)
+    {
+        super(name);
+        //super(color); // for later
+        this.price = price;
+    }
+    
+    public void setMaster(int master)
+    {
+        this.master = master; 
+    }
+    
+    public int getPrice()
+    {
+        return price;
+    }
+    
+    @Override 
+    public void action(Player player, Board board)
+    {
+        if (master < 0)
+        {
+        System.out.println(player.getName() + " do you want to purchase " + getName() + "?");
+        if //fuck TODO: handle player input on whether to buy property or not
+                // also charge rent if the player is not the Master/owner
+        }
+    }
+}
