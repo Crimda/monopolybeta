f390204b8ac6604faa4aa4fa8f3d50d8a6ac6861
Crimda <theblues92@gmail.com>
2015-12-07 16:05:20 -0500

    jail and parking

---
+++ monopoly/src/Jail.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brad
+ */
+public class Jail extends Space 
+{   
+    public Jail(String name) 
+    {
+	super(name);
+    }
+	
+    @Override
+    public void action(Player player, Board board) 
+    {
+    System.out.println("I am placeholder, ignore");
+    }
+}
