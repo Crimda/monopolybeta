4d5e5d498265a4e00c5ff7285f27b5edee4b027d
Wazubaba <wazubaba@wazu.info.tm>
2015-12-07 15:50:13 -0600

    Implemented jail-time handler

--- monopoly/src/GoToJail.java
+++ monopoly/src/GoToJail.java
@@ -6,14 +6,14 @@ public class GoToJail extends Space
 {
     public GoToJail(String name) 
     {
-	super(name);
+		super(name);
     }
 	
     @Override
     public void action(Player player, Board board) 
     {
-    System.out.println("Player has to go to jail");
-    player.setPos(10);
-    //might need to subtract money from player if the game requires a fine 
+		System.out.println("Player has to go to jail");
+	    player.setPos(10);
+	    player.setInJail();
     }
 }
--- monopoly/src/Player.java
+++ monopoly/src/Player.java
@@ -14,6 +14,8 @@ public class Player
     Money money = new Money(1500); //look up starting cash for real game
 
 	boolean inJail = false;
+	int escapeAttempts = 0;
+	boolean hasGetOutOfJailFreeCard = false;
 
     int numRailroads = 0;
     int numUtilities = 0;
@@ -54,6 +56,11 @@ public class Player
     	}
     }
     
+	public void setInJail()
+	{
+		this.inJail = true;
+	}
+
     public int getPos()
     {
         return position;
@@ -73,6 +80,78 @@ public class Player
     {
 		return playerID;
     }
+
+    public boolean jailTime()
+    { // Returns false if a player attempted an invalid action, true otherwise
+    	if (!this.inJail) return;
+
+    	int choice = 0;
+
+		// TODO: Get player choice; 1 == roll, 2 == pay, 3 == use card(if possible), else == invalid
+		
+		if (this.escapeAttempts == 3)
+		{
+			this.money.subtractMoney(50);
+			this.inJail = false;
+			return true;
+		}
+
+    	if (choice == 1)
+    	{
+    		if (this.escapeAttempts < 3)
+    		{
+    			this.escapeAttempts += 1;
+    			int[3] dieRoll = Dice.getRoll();
+    			if (dieRoll[0] == dieRoll[1])
+    			{
+    				this.inJail = false;
+    				// TODO: Move player post this
+    			}
+    			else
+    			{
+    				// TODO: inform player that they failed to escape
+    			}
+    		}
+    		return true;
+    	} else
+    	if (choice == 2)
+    	{
+    		if (this.escapeAttempts == 0)
+    		{
+	    		this.money.subtractMoney(50);
+				this.inJail = false;
+				return true;
+			}
+			else
+			{
+				// TODO: inform player that they may not roll again
+				return false;
+			}	
+		} else
+		if (choice == 3)
+		{
+			if (this.escapeAttempts == 0)
+			{
+				if (this.hasGetOutOfJailFreeCard)
+				{
+					this.hasGetOutOfJailFreeCard = false;
+					this.inJail = false;
+					return true;
+				}
+				else
+				{
+					// TODO: inform player that they lack a get out of jail free card
+					return false;
+				}
+			}
+			else
+			{
+				// TODO: inform player that they may not use the get out of jail free card after rolling
+				return false;
+			}
+		}
+    }
+
     
     public int rollDice()
     { // Roll the fookin dice
