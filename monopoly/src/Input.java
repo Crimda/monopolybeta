cd508f9a7f876197e3aa0853a33ffaea2031699a
Crimda <theblues92@gmail.com>
2015-12-09 17:22:28 -0500

    Revert "added int validation"
    This reverts commit 1250be108aea502c6248be701084ec90d09dd477.

--- monopoly/src/Input.java
+++ monopoly/src/Input.java
@@ -13,25 +13,4 @@ public class Input
         return s;
     }
 
-   public static int getInt(Scanner sc, String prompt)
-    {
-        boolean isValid = false;
-        int i = 0;
-        while (isValid == false)
-        {
-            System.out.print(prompt);
-            if (sc.hasNextInt())
-            {
-                i = sc.nextInt();
-                isValid = true;
-            }
-            else
-            {
-                System.out.println("Error! Invalid integer value. Try again.");
-            }
-            sc.nextLine();  // discard any other data entered on the line
-        }
-        return i;
-    }    
 }
-
