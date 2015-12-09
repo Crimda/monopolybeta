1250be108aea502c6248be701084ec90d09dd477
Crimda <theblues92@gmail.com>
2015-12-09 16:46:11 -0500

    added int validation

--- monopoly/src/input.java
+++ monopoly/src/input.java
@@ -13,4 +13,25 @@ public class input
         return s;
     }
 
+   public static int getInt(Scanner sc, String prompt)
+    {
+        boolean isValid = false;
+        int i = 0;
+        while (isValid == false)
+        {
+            System.out.print(prompt);
+            if (sc.hasNextInt())
+            {
+                i = sc.nextInt();
+                isValid = true;
+            }
+            else
+            {
+                System.out.println("Error! Invalid integer value. Try again.");
+            }
+            sc.nextLine();  // discard any other data entered on the line
+        }
+        return i;
+    }    
 }
+
